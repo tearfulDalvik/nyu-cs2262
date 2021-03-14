b0341e72b32cb47ea72aa279f1f2b42f2a2db13c
Gufeng <meizaizheli@ifengge.cn>
2021-03-14 22:53:14 +0800

    my commit

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from datetime import datetime
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+@app.route('/time')
+def time():
+    return datetime.now().strftime("%H:%M:%S")
 
 
 app.run(host='0.0.0.0',
